15f31ab66a71addd3e8b25228e84eb5d60dff08a
bowiego <295030485@qq.com>
2019-08-16 17:28:54 +0800

    modify

--- eshop_price.py
+++ eshop_price.py
@@ -1,10 +1,31 @@
 import requests
+import json
 from lxml import etree
 
-def getPrices():
-  html = requests.get("https://blog.csdn.net/it_xf?viewmode=contents")
+html = requests.get('https://eshop-prices.com/prices?currency=CNY')
+etree_html = etree.HTML(html.text)
+# tr = etree_html.xpath('//*[@class="prices"]/table/tbody/tr[%s]' % (i))
+
+def getPrices(limit = 1):
+  offset = 1
+  result = []
   
-  etree_html = etree.HTML(html.text)
-  content = etree_html.xpath('//*[@id="mainBox"]/main/div[2]/div[1]/h4/a/text()')
-  for each in content:
-    print(each)
+  def getData(offset, limit):
+    print(limit * offset, limit * (offset + 1))
+    for i in range(limit * offset, limit * (offset + 1)):
+      print(i)
+      title = etree_html.xpath('//*[@class="prices"]/table/tbody/tr[%s]/th[1]/a[1]/text()' % (i))[0]
+      prices = etree_html.xpath('//*[@class="prices"]/table/tbody/tr[%s]/td/text()' % (i))
+      item = {
+        'title': title,
+        'prices': prices
+      }
+      result.append(item)
+    
+  getData(offset, limit)
+    # result.append(item)
+
+    # print(content[i], etree_html.xpath('//*[@class="prices"]/table/tbody/tr[%s]/td/text()' % (i)))
+  # print(result)
+  with open('switch_all_game_title.json','w') as file:
+    file.write(json.dumps(result))
